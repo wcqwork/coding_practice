942d8fcf7607d4ea5acd46f850e37f132027dd5e
wangchengqiang <5673515+make_progress_every_day1@user.noreply.gitee.com>
2026-06-15 10:13:21 +0800

    insert

--- coding/2026.6.15/index.sql
+++ coding/2026.6.15/index.sql
@@ -1,21 +1,20 @@
 CREATE TABLE IF NOT EXISTS `phoenix_agent_showcase_featured` (
-	`showcase_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-	`organization_id` BIGINT NOT NULL,
-	`SITE_SETTING_ID` BIGINT NOT NULL COMMENT '网站ID',
-	`INDUSTRY_CODES` VARCHAR(255) NOT NULL COMMENT '新行业表编码（非ID）;例如：,1,1,',
-	`LOCATION_IDS` VARCHAR(255) NOT NULL COMMENT '地域表中主键ID（区/县级）;例如：,1,1,',
-	`site_start_time` TIMESTAMP NOT NULL,
-	`site_logon_time` TIMESTAMP NOT NULL,
-	`site_pv` INTEGER NOT NULL,
-	`site_uv` INTEGER NOT NULL,
-	`site_action` INTEGER NOT NULL,
-	`site_inquire` INTEGER NOT NULL,
-	`site_prod_num` INTEGER NOT NULL,
-	`site_article_num` INTEGER NOT NULL,
-	`add_time` TIMESTAMP NOT NULL,
-	PRIMARY KEY(`showcase_id`)
+  `showcase_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+  `organization_id` BIGINT NOT NULL,
+  `SITE_SETTING_ID` BIGINT NOT NULL COMMENT '网站ID',
+  `INDUSTRY_CODES` VARCHAR(255) NOT NULL COMMENT '新行业表编码（非ID）;例如：,1,1,',
+  `LOCATION_IDS` VARCHAR(255) NOT NULL COMMENT '地域表中主键ID（区/县级）;例如：,1,1,',
+  `site_start_time` TIMESTAMP NOT NULL,
+  `site_logon_time` TIMESTAMP NOT NULL,
+  `site_pv` INTEGER NOT NULL,
+  `site_uv` INTEGER NOT NULL,
+  `site_action` INTEGER NOT NULL,
+  `site_inquire` INTEGER NOT NULL,
+  `site_prod_num` INTEGER NOT NULL,
+  `site_article_num` INTEGER NOT NULL,
+  `add_time` TIMESTAMP NOT NULL,
+  PRIMARY KEY(`showcase_id`)
 );
-
 
 CREATE INDEX `phoenix_agent_showcase_featured_index_0`
 ON `phoenix_agent_showcase_featured` (`organization_id`);
